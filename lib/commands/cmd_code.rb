c82d2dc0eb07372cf45a850fcc0a34f24d44642b
Eugene Lapeko <eugene@lapeko.info>
2018-07-31 22:53:29 +0300

    Fix check for forgotten message

--- lib/commands/cmd_code.rb
+++ lib/commands/cmd_code.rb
@@ -4,7 +4,7 @@ module SND
   # Chat /list command processor
   module ChatCommand
     def cmd_code(msg)
-      return chat.send_noprefix unless msg =~ %r{^#}
+      return chat.send_noprefix unless msg =~ %r{^[#\/]}
       chat.send_message(
         text: chat.send_code(Unicode.downcase(msg[1..-1]), @time)
       )
