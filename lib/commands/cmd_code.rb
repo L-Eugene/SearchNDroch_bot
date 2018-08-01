5ce41f596e6256ebbb554229265b33bebbb1463b
Eugene Lapeko <eugene@lapeko.info>
2018-08-01 21:33:56 +0300

    Replace code prefix with ! sign (was #)

--- lib/commands/cmd_code.rb
+++ lib/commands/cmd_code.rb
@@ -5,7 +5,7 @@ module SND
   module ChatCommand
     def cmd_code(msg)
       return if msg =~ %r{^\/}
-      return chat.send_noprefix unless msg =~ %r{^#}
+      return chat.send_noprefix unless msg =~ %r{^!}
       chat.send_message(
         text: chat.send_code(Unicode.downcase(msg[1..-1]), @time)
       )
