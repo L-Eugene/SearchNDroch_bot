9002a1858578f086da37cefa3feba8706a60d2cb
Eugene Lapeko <eugene@lapeko.info>
2019-01-17 23:53:06 +0300

    Change debug options format.

--- lib/log/snd_logger.rb
+++ lib/log/snd_logger.rb
@@ -11,10 +11,10 @@ module SND
     attr_reader :logger
 
     def initialize
-      flag = SND.cfg.options['debug']
+      debug = SND.cfg.options['debug']['enabled'] || File.exist?(SND.cfg.options['debug']['flag'])
 
       @logger = Logger.new(SND.cfg.options['logfile'], 'daily')
-      @logger.level = File.exist?(flag) ? Logger::DEBUG : Logger::INFO
+      @logger.level = debug ? Logger::DEBUG : Logger::INFO
       @logger.formatter = proc do |severity, datetime, _progname, msg|
         date_format = datetime.strftime('%Y-%m-%d %H:%M:%S')
         "[#{date_format}] #{severity}: #{msg}\n"
