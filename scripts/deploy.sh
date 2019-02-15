d864fdd435330bb71ff3a9f571c6bd6d99e0f534
Eugene Lapeko <eugene@lapeko.info>
2019-02-16 00:19:26 +0300

    Move deployment logic to script.

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "development" ]; then
+  BOT_PATH=$BOT_PATH_DBG
+  BOTSERVER_PATH=$BOTSERVER_PATH_DBG
+  RVM_WRAPPER=$RVM_WRAPPER_DBG
+  SERVICE_NAME=$SERVICE_NAME_DBG
+elif [ "$1" != "development" ]; then
+  echo "Invalid argument"
+  exit 2
+fi
+
+commands=(
+  "cd $BOT_PATH; git pull"
+  "cd $BOTSERVER_PATH; $RVM_WRAPPER/bundle install --without test;"
+  "cd $BOTSERVER_PATH; $RVM_WRAPPER/bundle update"
+  "cd $BOTSERVER_PATH; $RVM_WRAPPER/rake snd:db:migrate"
+  "sudo service $SERVICE_NAME restart"
+)
+
+for cmd in "${commands[@]}"; do
+  ssh $DEPLOY_USER@$DEPLOY_SERVER "$cmd"
+done
