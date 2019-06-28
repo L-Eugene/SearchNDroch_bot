a91b118f166bc7f658f0e48f67e114ca54426e57
Eugene Lapeko <eugene@lapeko.info>
2019-06-28 12:51:21 +0300

    Add debug log to file upload routine.

--- lib/parser/snd_parser.rb
+++ lib/parser/snd_parser.rb
@@ -15,6 +15,7 @@ module SND
     # @param [String] ext
     # @return [Hash]
     def self.parse(file, ext, chat)
+      SND.log.debug { "#{file.path}: EXT=#{ext}" }
       parser = subclasses.find { |klass| klass.extensions.include? ext }
       raise SND::InvalidFileExtension unless parser
 
